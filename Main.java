de8b49234d3dc78f70c314af41ec892a2f60e6f5
Nisapiliang <khairunnisa.kn369@gmail.com>
2021-10-22 12:07:11 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,48 @@
+class Main {
+    public static void main(String[] args) {
+      Heroes hero = new Heroes();
+  
+      hero.setProfile("Khoirunnisyah", 21, 150);
+      hero.profile();
+      
+  
+      // jalankan aktivitas disini...
+      hero.jalan();
+      hero.jalan();
+      hero.jalan();
+      hero.jalan();
+      hero.jalan();
+  
+      hero.lari();
+      hero.lari();
+      hero.lari();
+      hero.lari();
+
+      hero.makan();
+      hero.makan();
+      hero.makan();
+      hero.makan();
+
+      hero.minum();
+      hero.minum();
+      hero.minum();
+      hero.minum();
+
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+
+      hero.duduk();
+      hero.duduk();
+
+
+
+
+
+
+      
+      // tampilkan status terkini...
+      hero.profile();
+      
+    }
+  }
